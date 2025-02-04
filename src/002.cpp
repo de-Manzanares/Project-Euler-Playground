e60cd0e2f1334cce5825772e4c3e725bb24392fc
de-Manzanares <schauss@dmanz.org>
2025-02-04 01:00:04 -0800

    002

---
+++ src/002.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+
+  int i{0};
+  int j{1};
+  int k = i + j;
+  int sum{};
+
+  while (k < 4E6) {
+    k = i + j;
+    i = j;
+    j = k;
+    if (k % 2 == 0) {
+      sum += k;
+    }
+  }
+
+  std::cout << sum;
+}
