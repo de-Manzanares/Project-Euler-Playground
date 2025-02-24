b3aaa95c94266f26754ace46e58e70066c228a9a
de-Manzanares <schauss@dmanz.org>
2025-02-23 16:49:59 -0800

    005

---
+++ src/005.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <limits>
+
+int main() {
+  for (int i = 20 * 19; i < INT_MAX; i += 2) {
+    for (int j = 2; j < 21; ++j) {
+      if (i % j != 0) {
+        break;
+      } else if (j == 20) {
+        std::cout << i;
+      }
+    }
+  }
+}
